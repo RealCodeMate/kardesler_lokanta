0f8b154559d5b0a03a5f824d586396dec0030632
Gürkan <gurkanyigitgr@gmail.com>
2023-04-11 02:41:22 +0300

    completed menu section

---
+++ kardesler_lokanta/src/components/Menu.jsx
@@ -0,0 +1,427 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div id="menü" className=" grid grid-cols-3 xs:grid-cols-1">
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/4.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/7.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/8.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/1.png"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/2.png"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/3.png"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/10.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/11.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/12.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/16.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/19.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/6.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/15.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/13.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/14.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/18.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/20.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+      <div className="hero container max-w-screen-lg mx-auto pb-10 flex flex-col">
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto"
+        />
+        <img
+          src={
+            "https://github.com/RealCodeMate/kardesler_lokanta/blob/main/kardesler_lokanta/src/assets/menus/21.png?raw=true"
+          }
+          alt="fire"
+          className="w-[20rem] h-[15rem] mx-auto mt-7 mb-7 cursor-pointer transition duration-[2800ms] ease-in-out hover:-rotate-180 z-10 "
+        />
+        <img
+          src={
+            "https://raw.githubusercontent.com/RealCodeMate/kardesler_lokanta/main/kardesler_lokanta/src/assets/menus/fire_2.jpg"
+          }
+          alt="fire"
+          className="w-[30rem] h-[15rem] animate-pulse mx-auto rotate-180"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
